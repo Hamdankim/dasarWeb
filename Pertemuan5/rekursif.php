69e2c84ef2f44ad9df1b95e1ac147fcbbdf4c157
haulim <hamdanizul24@gmail.com>
2024-09-26 19:39:26 +0700

    Pertemuan5_ArrayRekursif

--- Pertemuan5/rekursif.php
+++ Pertemuan5/rekursif.php
@@ -1,21 +1,69 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia! <br>";
+// function tampilkanHaloDunia() {
+//     echo "Halo dunia! <br>";
 
-    tampilkanHaloDunia();
-}
-// tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+// // tampilkanHaloDunia();
 
-for ($i=1; $i <= 25; $i++) { 
-    echo "Perulangan ke-{$i} <br>";
-}
-echo "<hr>";
-function tampilkanAngka (int $jumlah, int $indeks = 1) {
-    echo "Perulangan ke-{$indeks} <br>";
+// for ($i=1; $i <= 25; $i++) { 
+//     echo "Perulangan ke-{$i} <br>";
+// }
+// echo "<hr>";
+// function tampilkanAngka (int $jumlah, int $indeks = 1) {
+//     echo "Perulangan ke-{$indeks} <br>";
 
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks+1);
+//     if ($indeks < $jumlah) {
+//         tampilkanAngka($jumlah, $indeks+1);
+//     }
+// }
+// tampilkanAngka(20);
+
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+function tampilkanMenuBertingkat(array $menu)
+{
+    echo "<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}</li>";
+
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
     }
+    echo "</ul>";
 }
-tampilkanAngka(20);
-?>
+
+tampilkanMenuBertingkat($menu);
