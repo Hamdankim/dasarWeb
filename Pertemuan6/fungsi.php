59987f85fc47013056fec6efbfd087e2745109f0
haulim <hamdanizul24@gmail.com>
2024-10-04 08:20:51 +0700

    Pertemuan6_Fungsi_Soal4

---
+++ Pertemuan6/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Hamdan.<br/>";
+    echo "Senang berkenalan dengan anda.<br/>";
+}
+
+perkenalan();
+echo "<br/>";
+perkenalan();
+?>
