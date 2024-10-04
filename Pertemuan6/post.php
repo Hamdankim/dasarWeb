6f9a3664d25cdb5b7da84ac4d635b0e5946c8b02
haulim <hamdanizul24@gmail.com>
2024-10-04 19:23:05 +0700

    Pertemuan6_POST_Soal24

---
+++ Pertemuan6/post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        Name: <input type="text" name="name">
+        <input type="submit">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_REQUEST['name'];
+        if (empty($name)) {
+            echo "Name is empty";
+        } else {
+            echo $name;
+        }
+    }
+    ?>
+</body>
+
+</html>
