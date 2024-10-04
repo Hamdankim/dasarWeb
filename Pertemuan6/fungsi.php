0fe23541b15eba72b306233379d8a22a061badd6
haulim <hamdanizul24@gmail.com>
2024-10-04 08:36:01 +0700

    Pertemuan6_FungsiMengembalikanNilai_Soal7

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -14,4 +14,11 @@ $saya = "Hamdan";
 $ucapanSalam = "Selamat pagi";
 
 perkenalan($saya);
+
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang-$thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2004, 2024) . " tahun";
 ?>
