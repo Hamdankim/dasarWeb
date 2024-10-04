0a695e4932356b9724b82cfb0db62acedf6c7d8d
haulim <hamdanizul24@gmail.com>
2024-10-04 08:41:18 +0700

    Pertemuan6_MemanggilFungsiDalamFungsi_Soal8

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,24 +1,15 @@
 <?php
-
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
-}
-
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Hamdan";
-$ucapanSalam = "Selamat pagi";
-
-perkenalan($saya);
-
 function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang-$thn_lahir;
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungUmur(2004, 2024) . " tahun";
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Umur saya adalah ". hitungUmur(2004, 2024) . " tahun<br>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan("Hamdan");
 ?>
