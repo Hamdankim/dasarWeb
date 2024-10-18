3e22aaf32cf94c57a89f164fb95b9709762f30fe
haulim <hamdanizul24@gmail.com>
2024-10-18 09:11:27 +0700

    Praktikum4_Soal12

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,14 +1,13 @@
 <?php
-if (isset($_POST['submit'])) {
     $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES['myfile']['name']);
+    $targetfile = $targetdir . basename($_FILES['file']['name']);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
     $allowedExtensions = array('txt', 'pdf', 'doc', 'docx');
     $maxsize = 3 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
-        if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
+    if (in_array($fileType, $allowedExtensions) && $_FILES['file']['size'] <= $maxsize) {
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $targetfile)) {
             echo "File berhasil diunggah";
             echo "<br>";
             echo '<img src="./uploads/download.jpeg" width="200">';
@@ -18,5 +17,4 @@ if (isset($_POST['submit'])) {
     } else {
         echo "File tidak valid atau melebihi ukuran maksimal";
     }
-}
 ?>
