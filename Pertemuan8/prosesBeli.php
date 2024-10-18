f22a527141f2cae691a6fa5b0afb8e420fddd05c
haulim <hamdanizul24@gmail.com>
2024-10-18 21:35:28 +0700

    Praktikum8_Soal20

--- Pertemuan8/prosesBeli.php
+++ Pertemuan8/prosesBeli.php
@@ -2,5 +2,5 @@
 if (isset($_POST['beliNovel']) && isset($_POST['beliBuku'])) {
     setcookie("beliNovel", $_POST["beliNovel"]);
     setcookie("beliBuku", $_POST["beliBuku"]);
-    header("Location: keranjangBelanja.php");
+    header("location: keranjangBelanja.php");
 }
